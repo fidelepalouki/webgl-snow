330d8ebe6be824ed453ee72291cfa5bac911ee70
Fidèle PALOUKI <fidele.palouki@gmail.com>
2018-12-09 23:48:32 +0100

    working example but with low fps

--- wegl-utils.js
+++ wegl-utils.js
@@ -1,193 +1,72 @@
-import { mat3 } from "gl-matrix/lib/gl-matrix.js";
-import "./style.css";
-import {
-  vertexShaderSource,
-  fragmentShaderSource,
-  flakeVertexShaderSource,
-  flakeFragmentShaderSource,
-  createShader,
-  createProgram
-} from "./webgl-utils.js";
+export const vertexShaderSource = `#version 300 es
+  in mat3 a_transformMatrix;
+  in vec4 a_color;
 
-class SnowFlake {
-  constructor(gl, x, y, r, vx, vy) {
-    this.gl = gl;
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.vx = vx;
-    this.vy = vy;
+  out vec4 v_color;
+
+  void main() {
+    gl_Position = vec4((a_transformMatrix * vec3(0, 0, 1)).xy, 0, 1);
+    // v_color = a_color;
+    v_color = vec4(15, 56, 155, 1);
   }
+`;
 
-  get transformMatrix() {
-    const translation = [this.x, this.y];
-    const rotation = this.randBetween(0, 2 * Math.PI);
-    const scale = [this.r, this.r];
-    const velocity = [this.vx, this.vy];
+export const fragmentShaderSource = `#version 300 es
+  precision mediump float;
 
-    let matrix = [];
-    mat3.identity(matrix);
-    mat3.projection(
-      matrix,
-      this.gl.canvas.clientWidth,
-      this.gl.canvas.clientHeight
-    );
-    mat3.translate(matrix, matrix, translation);
-    mat3.rotate(matrix, matrix, rotation);
-    mat3.scale(matrix, matrix, scale);
-    return matrix;
+  in vec4 v_color;
+  out vec4 outColor;
+
+  void main() {
+    outColor = v_color;
   }
+`;
 
-  vertices() {
-    let vertices = [];
-    for (let i = 0; i <= 360; i++) {
-      const angle = (i * Math.PI) / 180;
-      const vertice1 = [
-        this.x + this.r * Math.cos(angle),
-        this.y + this.r * Math.sin(angle)
-      ];
-      const vertice2 = [this.x, this.y];
-      vertices = [...vertices, ...vertice1, ...vertice2];
-    }
-    return vertices;
+export const flakeVertexShaderSource = `#version 300 es
+  in vec2 a_vertice;
+  in vec4 a_color;
+  uniform mat3 u_transformMatrix;
+
+  out vec4 v_color;
+
+  void main() {
+    v_color = vec4(0.8, 0.4, 0.8, 1);
+    gl_Position = vec4((u_transformMatrix * vec3(a_vertice, 1)).xy, 0, 1);
   }
+`;
 
-  randBetween(min, max) {
-    return min + (max - min) * Math.random();
+export const flakeFragmentShaderSource = `#version 300 es
+  precision mediump float;
+
+  in vec4 v_color;
+  out vec4 outColor;
+
+  void main() {
+    outColor = v_color;
   }
+`;
 
-  update(gl) {
-    const vertexShader = createShader(
-      gl,
-      gl.VERTEX_SHADER,
-      flakeVertexShaderSource
-    );
-    const fragmentShader = createShader(
-      gl,
-      gl.FRAGMENT_SHADER,
-      flakeFragmentShaderSource
-    );
-    const program = createProgram(gl, vertexShader, fragmentShader);
-
-    const verticeAttribLocation = gl.getAttribLocation(program, "a_vertice");
-
-    const vertexArrayObject = gl.createVertexArray();
-    gl.bindVertexArray(vertexArrayObject);
-
-    const verticeBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, verticeBuffer);
-    // console.log('vertices', this.vertices());
-
-    const vertices = this.vertices();
-
-    gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(this.vertices()),
-      gl.STATIC_DRAW
-    );
-    gl.enableVertexAttribArray(verticeBuffer);
-    const [size, type, normalize, stride, offset] = [2, gl.FLOAT, false, 0, 0];
-    gl.vertexAttribPointer(
-      verticeAttribLocation,
-      size,
-      type,
-      normalize,
-      stride,
-      offset
-    );
-
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
-
-    gl.useProgram(program);
-    gl.bindVertexArray(vertexArrayObject);
-    const [mode, offset, count] = [gl.TRIANGLE_STRIP, 0, vertices.length];
-    gl.drawArrays(mode, offset, count);
+export function createShader(gl, type, shaderSource) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
   }
+  return shader;
 }
 
-class Snow {
-  constructor() {
-    this.canvas = document.createElement("canvas");
-    document.querySelector("body").appendChild(this.canvas);
-    this.gl = this.canvas.getContext("webgl2");
-
-    this.addListeners();
-    this.createSnowFlakes();
-    requestAnimationFrame(this.update.bind(this, this.gl));
+export function createProgram(gl, vertexShader, fragmentShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return null;
   }
-
-  addListeners() {
-    this.onResize();
-    window.addEventListener("resize", () => this.onResize());
-  }
-
-  onResize() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-  }
-
-  randBetween(min, max) {
-    return min + (max - min) * Math.random();
-  }
-
-  createSnowFlakes() {
-    const flakes = window.innerWidth / 4;
-    this.snowFlakes = [];
-    for (let i = 0; i < flakes; i++) {
-      const [x, y] = [
-        this.randBetween(0, window.innerWidth),
-        this.randBetween(0, -window.innerHeight)
-      ];
-      const r = this.randBetween(1, 4);
-      const [vx, vy] = [this.randBetween(-3, 3), this.randBetween(2, 5)];
-      this.snowFlakes.push(new SnowFlake(this.gl, x, y, r, vx, vy));
-    }
-  }
-
-  update(gl) {
-    // const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    // const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-    // const program = createProgram(gl, vertexShader, fragmentShader);
-
-    // const transformMatrixAttribLocation = gl.getAttribLocation(program, 'a_transformMatrix');
-
-    // const vertexArrayObject = gl.createVertexArray();
-    // gl.bindVertexArray(vertexArrayObject);
-
-    // const transformMatrixBuffer = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER, transformMatrixBuffer);
-
-    // let transformMatrixData = [];
-    // this.snowFlakes.forEach(snowFlake => {
-    //   transformMatrixData = [
-    //     ...transformMatrixData,
-    //     ...snowFlake.transformMatrix
-    //   ];
-    // });
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(transformMatrixData), gl.STATIC_DRAW);
-    // gl.enableVertexAttribArray(transformMatrixBuffer);
-    // const [size, type, normalize, stride, offset] = [9, gl.FLOAT, false, 0, 0];
-    // gl.vertexAttribPointer(transformMatrixAttribLocation, size, type, normalize, stride, offset);
-
-    // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    // gl.clearColor(0, 0, 0, 0);
-    // gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
-
-    // gl.useProgram(program);
-    // gl.bindVertexArray(vertexArrayObject);
-    // const [mode, offset, count] = [gl.TRIANGLE_STRIP, 0, 9];
-    // gl.drawArrays(mode, offset, count);
-
-    // for (const flake of this.snowFlakes) {
-    //   flake.update(this.gl);
-    // }
-
-    this.snowFlakes[0].update(this.gl);
-
-    // requestAnimationFrame(this.update.bind(this, gl));
-  }
+  return program;
 }
-
-new Snow();
